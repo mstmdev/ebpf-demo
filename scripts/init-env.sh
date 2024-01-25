c1123fed45eae3f37de0a25277324e3759fe623a
mstmdev <mstmdev@gmail.com>
2024-01-25 16:16:05 +0800

    Reflect

--- scripts/init-env.sh
+++ scripts/init-env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 sudo apt-get install llvm
-sudo ln -sf /usr/include/asm-generic/ /usr/include/asm
 sudo apt-get install libbpf-dev
+sudo ln -sf /usr/include/asm-generic/ /usr/include/asm
